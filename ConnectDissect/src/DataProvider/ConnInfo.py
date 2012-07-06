8dd8790c68ede5082ec0bd845bcdf7e57cfa4556
moz <moz@leon>
2012-07-06 23:52:20 +0200

    Added conninfo datatype to facilitate comparison

---
+++ ConnectDissect/src/DataProvider/ConnInfo.py
@@ -0,0 +1,46 @@
+class ConnInfo():
+    _dip = ''
+    _sip = ''
+    _dprot = 0
+    _sport = 0
+    _TransportProtocol = ''
+       
+    def __init__(self, TcpFrame):
+        self._BuiltConnectionInfo( TcpFrame )
+
+        self.__dict__['src'] = (self._sip, self._sport)
+        self.__dict__['dst'] = (self._dip, self._dport)
+        self.__dict__['proto'] = self._TransportProtocol
+        pass
+
+# TODO: implement something like this
+#    def __setattr__(self, name, value):
+#        raise ValueError("It's read only!")
+        
+    def _BuiltConnectionInfo(self, pkg):
+        ''' only handles IP traffic '''
+        self._sip = pkg['IP'].src
+        self._dip = pkg['IP'].dst
+
+        if 'TCP' in pkg:
+            self._sport = pkg['TCP'].sport
+            self._dport = pkg['TCP'].dport
+            self._TransportProtocol = 'TCP'
+        elif 'UDP' in pkg:
+            self._sport = pkg['UDP'].sport
+            self._dport = pkg['UDP'].dport
+            self._TransportProtocol = 'UDP'
+        else:
+            raise ValueError('Package must contain IP and TCP/UDP')
+
+    def __eq__(self, other):
+        if not self.proto == other.proto:
+            return False
+        
+        if self.src == other.src and self.dst == other.dst:
+            return True
+        
+        if self.dst == other.src and self.src == other.dst:
+            return True
+        
+        raise ValueError( "something wnet wrong in comparison")
---
+++ ConnectDissect/src/tests/testConnInfo.py
@@ -0,0 +1,34 @@
+'''
+Created on Jul 6, 2012
+
+@author: moz
+'''
+
+import unittest
+from scapy.all import * #@UnusedWildImport
+
+from DataProvider.ConnInfo import ConnInfo
+
+TcpFrame = IP( src='127.0.0.1', dst='192.168.1.1')/TCP( dport=80, sport=1120) #@UndefinedVariable
+TcpFrameSource = ('127.0.0.1', 1120 )
+TcpFrameDestination = ('192.168.1.1', 80 )
+TcpFrameProtocol = 'TCP'
+
+TcpFrameReply = IP( dst='127.0.0.1', src='192.168.1.1')/TCP( sport=80, dport=1120) #@UndefinedVariable
+
+class Test(unittest.TestCase):
+    def testConnInfoConstructor(self):
+        CI = ConnInfo( TcpFrame )
+        self.assertEqual( CI.src,TcpFrameSource )
+        self.assertEqual( CI.dst,TcpFrameDestination )
+        self.assertEqual( CI.proto,TcpFrameProtocol )
+        pass
+
+    def testEqual(self):
+        CI = ConnInfo( TcpFrame )
+        self.assertEqual( CI, CI)
+        
+    def testReplyEqual(self):
+        CI = ConnInfo( TcpFrame )
+        CI_reply = ConnInfo( TcpFrameReply )
+        self.assertEqual( CI, CI_reply)
