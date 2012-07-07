5639ed4c868fdac0241934117a1eaeeee57e4a3d
moz <moz@leon>
2012-07-07 21:11:40 +0200

    DnsReader now saves entries seen

--- ConnectDissect/src/ConnReaders/DnsReader.py
+++ ConnectDissect/src/ConnReaders/DnsReader.py
@@ -1,4 +1,5 @@
 class DnsReader():
+    _Entries = []
 
     def ProcessPkg(self, DnsPkg):
         ''' no memory in this reader. '''
@@ -7,8 +8,15 @@ class DnsReader():
         
         if not DnsPkg['DNS'].an:
             return None
-        
+
+        self._Entries.append( (DnsPkg['DNS'].an.rrname,  DnsPkg['DNS'].an.rdata) )
         return ('DNS', "%s\t%s"%(DnsPkg['DNS'].an.rrname,  DnsPkg['DNS'].an.rdata) )
+
+    
+    def GetDnsEntries(self):
+        return self._Entries
+    
+    
     
     
 
--- ConnectDissect/src/tests/testDnsReader.py
+++ ConnectDissect/src/tests/testDnsReader.py
@@ -10,17 +10,21 @@ from ConnReaders.DnsReader import DnsReader
 DnsRequestPacket =      IP(dst="192.168.5.1")/UDP()/DNS(rd=1,qd=DNSQR(qname="www.slashdot.org")) #@UndefinedVariable
 DnsResponsePacket =     IP(dst="192.168.5.1")/UDP()/DNS(rd=1,qd=DNSQR(qname="www.slashdot.org"), an = DNSRR(rrname = "www.slashdot.org", rdata = "31.33.7.31", type='A')) #@UndefinedVariable
 ResponseEventText = ('DNS', "www.slashdot.org\t31.33.7.31")
+DnsEntries = [('www.slashdot.org', '31.33.7.31')]
 
 class Test(unittest.TestCase):
     def testQueryPkg(self):
         DR = DnsReader()
         Event = DR.ProcessPkg(DnsRequestPacket )
         self.assertEqual( Event, None )
+        self.assertEqual( DR.GetDnsEntries(), [])
 
     def testRespPkg(self):
         DR = DnsReader()
         Event = DR.ProcessPkg(DnsResponsePacket )
         self.assertEqual( Event, ResponseEventText )
+        self.assertEqual( DR.GetDnsEntries(), DnsEntries)
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
